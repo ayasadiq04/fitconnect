6a742aaaf333a4f84d89f56cbc4e13da00bc8c7c
ayasadiq04 <aya04sadiq@gmail.com>
2026-07-02 20:51:55 +0100

    Refactor project structure and add views

--- fitconnect.sql
+++ fitconnect.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2026 at 03:56 PM
+-- Generation Time: Jun 26, 2026 at 04:27 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -32,7 +32,7 @@ CREATE TABLE `abonnement` (
   `type_abonnement` varchar(50) NOT NULL,
   `date_debut` date NOT NULL,
   `date_fin` date NOT NULL,
-  `statut_` varchar(50) NOT NULL,
+  `statut` varchar(50) NOT NULL,
   `id_adherent` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -40,19 +40,19 @@ CREATE TABLE `abonnement` (
 -- Dumping data for table `abonnement`
 --
 
-INSERT INTO `abonnement` (`id_abonnement`, `type_abonnement`, `date_debut`, `date_fin`, `statut_`, `id_adherent`) VALUES
+INSERT INTO `abonnement` (`id_abonnement`, `type_abonnement`, `date_debut`, `date_fin`, `statut`, `id_adherent`) VALUES
 (1, 'Mensuel', '2026-01-10', '2026-02-10', 'Actif', 1),
 (2, 'Trimestriel', '2026-02-15', '2026-05-15', 'Actif', 2),
 (3, 'Annuel', '2026-03-05', '2027-03-05', 'Actif', 3),
-(4, 'Mensuel', '2026-03-20', '2026-04-20', 'Expire', 4);
+(4, 'Mensuel', '2026-04-20', '2026-05-20', 'Expiré', 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `adhérent`
+-- Table structure for table `adherent`
 --
 
-CREATE TABLE `adhérent` (
+CREATE TABLE `adherent` (
   `id_adherent` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
@@ -63,14 +63,14 @@ CREATE TABLE `adhérent` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `adhérent`
+-- Dumping data for table `adherent`
 --
 
-INSERT INTO `adhérent` (`id_adherent`, `nom`, `prenom`, `email`, `telephone`, `date_inscription`, `id_salle`) VALUES
-(1, 'Amine', 'El Idrissi', 'amine@gmail.com', '0612345678', '2026-01-10', 1),
-(2, 'Sara', 'Alaoui', 'sara@gmail.com', '0623456789', '2026-02-15', 1),
-(3, 'Youssef', 'Bennani', 'youssef@gmail.com', '0634567890', '2026-03-05', 2),
-(4, 'Nadia', 'Amrani', 'nadia@gmail.com', '0645678901', '2026-03-20', 2);
+INSERT INTO `adherent` (`id_adherent`, `nom`, `prenom`, `email`, `telephone`, `date_inscription`, `id_salle`) VALUES
+(1, 'Amrani', 'Sara', 'sara@gmail.com', '0612345678', '2026-01-10', 1),
+(2, 'Alaoui', 'Youssef', 'youssef@gmail.com', '0623456789', '2026-02-15', 2),
+(3, 'Bennani', 'Lina', 'lina@gmail.com', '0634567890', '2026-03-05', 3),
+(4, 'Tazi', 'Omar', 'omar@gmail.com', '0645678901', '2026-04-20', 4);
 
 -- --------------------------------------------------------
 
@@ -90,16 +90,18 @@ CREATE TABLE `salle` (
 --
 
 INSERT INTO `salle` (`id_salle`, `nom_salle`, `adresse`, `telephone`) VALUES
-(1, 'Fitness Center', 'Beni Mellal Centre', '0523485678'),
-(2, 'Power Gym', 'Quartier Al Massira', '0523498765');
+(1, 'Fitness Pro', 'Rue Hassan II', '0523456789'),
+(2, 'Power Gym', 'Avenue Mohammed V', '0523987654'),
+(3, 'Elite Sport', 'Quartier Al Qods', '0523123456'),
+(4, 'Body Center', 'Rue Ibn Sina', '0523765432');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `séance`
+-- Table structure for table `seance`
 --
 
-CREATE TABLE `séance` (
+CREATE TABLE `seance` (
   `id_seance` int(11) NOT NULL,
   `date_seance` date NOT NULL,
   `type_activite` varchar(50) NOT NULL,
@@ -110,14 +112,14 @@ CREATE TABLE `séance` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `séance`
+-- Dumping data for table `seance`
 --
 
-INSERT INTO `séance` (`id_seance`, `date_seance`, `type_activite`, `equipement`, `duree`, `id_salle`, `id_adherent`) VALUES
-(1, '2026-04-01', 'Musculation', 'Haltères', '01:30:00', 1, 1),
-(2, '2026-04-02', 'Cardio', 'Tapis roulant', '01:00:00', 1, 2),
-(3, '2026-04-03', 'Fitness', 'Vélo elliptique', '01:15:00', 2, 3),
-(4, '2026-04-04', 'Yoga', 'Tapis yoga', '01:00:00', 2, 4);
+INSERT INTO `seance` (`id_seance`, `date_seance`, `type_activite`, `equipement`, `duree`, `id_salle`, `id_adherent`) VALUES
+(1, '2026-06-01', 'Musculation', 'Haltères', '01:30:00', 1, 1),
+(2, '2026-06-05', 'Cardio', 'Tapis roulant', '01:00:00', 2, 2),
+(3, '2026-06-10', 'Fitness', 'Vélo elliptique', '01:15:00', 3, 3),
+(4, '2026-06-15', 'Crossfit', 'Barre olympique', '01:45:00', 4, 4);
 
 --
 -- Indexes for dumped tables
@@ -131,9 +133,9 @@ ALTER TABLE `abonnement`
   ADD KEY `id_adherent` (`id_adherent`);
 
 --
--- Indexes for table `adhérent`
+-- Indexes for table `adherent`
 --
-ALTER TABLE `adhérent`
+ALTER TABLE `adherent`
   ADD PRIMARY KEY (`id_adherent`),
   ADD KEY `id_salle` (`id_salle`);
 
@@ -144,12 +146,40 @@ ALTER TABLE `salle`
   ADD PRIMARY KEY (`id_salle`);
 
 --
--- Indexes for table `séance`
+-- Indexes for table `seance`
 --
-ALTER TABLE `séance`
+ALTER TABLE `seance`
   ADD PRIMARY KEY (`id_seance`),
   ADD KEY `id_salle` (`id_salle`),
   ADD KEY `id_adherent` (`id_adherent`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `abonnement`
+--
+ALTER TABLE `abonnement`
+  MODIFY `id_abonnement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `adherent`
+--
+ALTER TABLE `adherent`
+  MODIFY `id_adherent` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `salle`
+--
+ALTER TABLE `salle`
+  MODIFY `id_salle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `seance`
+--
+ALTER TABLE `seance`
+  MODIFY `id_seance` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -159,20 +189,20 @@ ALTER TABLE `séance`
 -- Constraints for table `abonnement`
 --
 ALTER TABLE `abonnement`
-  ADD CONSTRAINT `abonnement_ibfk_1` FOREIGN KEY (`id_adherent`) REFERENCES `adhérent` (`id_adherent`);
+  ADD CONSTRAINT `abonnement_ibfk_1` FOREIGN KEY (`id_adherent`) REFERENCES `adherent` (`id_adherent`);
 
 --
--- Constraints for table `adhérent`
+-- Constraints for table `adherent`
 --
-ALTER TABLE `adhérent`
-  ADD CONSTRAINT `adhérent_ibfk_1` FOREIGN KEY (`id_salle`) REFERENCES `salle` (`id_salle`);
+ALTER TABLE `adherent`
+  ADD CONSTRAINT `adherent_ibfk_1` FOREIGN KEY (`id_salle`) REFERENCES `salle` (`id_salle`);
 
 --
--- Constraints for table `séance`
+-- Constraints for table `seance`
 --
-ALTER TABLE `séance`
-  ADD CONSTRAINT `séance_ibfk_1` FOREIGN KEY (`id_salle`) REFERENCES `salle` (`id_salle`),
-  ADD CONSTRAINT `séance_ibfk_2` FOREIGN KEY (`id_adherent`) REFERENCES `adhérent` (`id_adherent`);
+ALTER TABLE `seance`
+  ADD CONSTRAINT `seance_ibfk_1` FOREIGN KEY (`id_salle`) REFERENCES `salle` (`id_salle`),
+  ADD CONSTRAINT `seance_ibfk_2` FOREIGN KEY (`id_adherent`) REFERENCES `adherent` (`id_adherent`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
